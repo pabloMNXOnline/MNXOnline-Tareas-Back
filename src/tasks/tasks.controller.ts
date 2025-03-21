e75d6323d977840498f515d119ddaa7fc8b55204
pabloMNXOnline <pablo@mnxonline.com>
2025-03-21 12:08:45 +0000

    Controlador de tasks terminado

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { RegisterTask } from './tasks.dto';
 import { TasksService } from './tasks.service';
 
@@ -14,8 +14,45 @@ export class TasksController {
     //Registrar una tarea: 
     @Post()
     async create(@Body() tasks:RegisterTask){
-        return 'Se ha registrado una tarea correctamente'
+        return {
+            name: tasks.name,
+            description: tasks.description,
+            colaborator_id: tasks.colaborator_id,
+            status_id: tasks.status_id,
+            message: 'Se ha registrado una tarea correctamente',
+          };
     }
 
+    //Modificar una tarea existente:
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() tasks:RegisterTask) {
+    const existingTask = this.tasksService.findOne(id);
 
+    if (!existingTask) {
+      return { message: 'Tarea no encontrada' };
+    }
+
+    this.tasksService.update(id, tasks);
+
+    return {
+        name: tasks.name,
+        description: tasks.description,
+        colaborator_id: tasks.colaborator_id,
+        status_id: tasks.status_id,
+        message: 'Tarea actualizada correctamente',
+      };
+  }
+
+    @Delete(':id') // Corrección aquí
+    async remove(@Param('id') id: number){ // Corrección aquí
+        const task = this.tasksService.findOne(id);
+        if (!task) {
+            return { message: 'Tarea no encontrada' };
+        }
+
+        this.tasksService.remove(id);
+
+        return { message: 'Tarea eliminada correctamente' };
+    }
 }
+
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,10 +4,40 @@ import { Injectable } from '@nestjs/common';
 export class TasksService {
 
     public tasks = [
-        { id: 1, title: 'Tarea 1', description: 'Descripción de la tarea 1' },
-        { id: 2, title: 'Tarea 2', description: 'Descripción de la tarea 2' },
-        { id: 3, title: 'Tarea 3', description: 'Descripción de la tarea 3' },
-        { id: 4, title: 'Tarea 3', description: 'Descripción de la tarea 4' },
+        { id: 1, name: 'Tarea 1', description: 'Descripción de la tarea 1', colaborator_id: 1, status_id:1 },
+        { id: 2, name: 'Tarea 2', description: 'Descripción de la tarea 2', colaborator_id: 1, status_id:1 },
+        { id: 3, name: 'Tarea 3', description: 'Descripción de la tarea 3', colaborator_id: 1, status_id:1 },
+        { id: 4, name: 'Tarea 4', description: 'Descripción de la tarea 4', colaborator_id: 1, status_id:1 },
     ];
 
+    //Esta funcion se cambiara en un futuro para llamadas a la api correspondiente
+
+    findOne(id:number): any {
+        console.log('Buscando tarea con ID:', id);
+        console.log(this.tasks.find((task) => task.id == id));
+        return this.tasks.find((task) => task.id == id);
+    }
+
+    update(id: number, updatedTask: any): void {
+        console.log('ID recibido:', id);
+        console.log('Datos recibidos:', updatedTask);
+        const taskIndex = this.tasks.findIndex((task) => task.id == id);
+        console.log('Índice encontrado:', taskIndex);
+        if (taskIndex !== -1) {
+          this.tasks[taskIndex] = {
+            ...this.tasks[taskIndex],
+            ...updatedTask,
+            id: this.tasks[taskIndex].id,
+          };
+          console.log('Tarea actualizada:', this.tasks[taskIndex]);
+        } else {
+          console.log('Tarea no encontrada');
+        }
+      }
+      remove(id: number): void {
+        const taskIndex = this.tasks.findIndex((task) => task.id == id);
+        if (taskIndex !== -1) {
+        this.tasks.splice(taskIndex, 1);
+      }
+      }
 }
