33eafd75b2e7da718f2d01b0d131fcd8c37dc3c7
pabloMNXOnline <pablo@mnxonline.com>
2025-03-21 12:14:56 +0000

    Controladores de todas las entidades sin hacer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ProjectsModule } from './projects/projects.module';
+import { CustomStatesModule } from './custom-states/custom-states.module';
+import { StatesModule } from './states/states.module';
+import { LabelsModule } from './labels/labels.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, ProjectsModule, CustomStatesModule, StatesModule, LabelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
