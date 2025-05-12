31f512a06123d788867221ec40a4f35f3ff64209
pabloMNXOnline <pablo@mnxonline.com>
2025-05-12 19:55:06 +0100

    aaa

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -21,8 +21,13 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { username: user.username, sub: user.id };
+    const token = this.jwtService.sign(payload);
+    console.log('user id ', user._id);
     return {
-      access_token: this.jwtService.sign(payload),
+    access_token: token,
+    user_id: user._id,
+    username:   user.username,
     };
+    
   }
 }
